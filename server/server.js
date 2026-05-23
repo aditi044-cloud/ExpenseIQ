25c96c87d041cb25b8636aea43874f2d7e214517
krish008-umt <krish008-umt@users.noreply.github.com>
2026-05-24 03:05:46 +0530

    Setup MongoDB Atlas connection and backend configuration

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const Expense = require("./models/Expense");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 // DATABASE CONNECTION
 mongoose.connect(process.env.MONGO_URI)
